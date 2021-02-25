3b1919cec69b184466e8cd196a18fdb1e254e97f
Carlos Vieira <appencigr@gmail.com>
2021-02-25 13:17:24 -0300

    Iniciei o JavaScript - Dia 4.1 - Exercícios

--- bloco4/dia1/script.js
+++ bloco4/dia1/script.js
@@ -1,1 +1,49 @@
+// Prática do Dia 4.1
+// Declaração de Variáveis
+// let num1 = 100;
+// let num2 = 22;
+// let num3 = 500;
+// Requisito 1
+// let somar = num1 + num2;
+// let subtrair = num2 - num1;
+// let multiplicar = num1 * num2;
+// let dividir = num1 * num2;
+// let modulo = ((num1 + num2) % 2) == 0;
+// Requisito 2
+// if (num1 > num2){
+//   console.log(`O maior é ${num1}`);
+// }else{
+//   console.log(`O maior é ${num2}`);
+// }
+// Requisito 3
+// if (num1 > num2 && num1 > num3){
+//   console.log(`O maior é ${num1}`);
+// }else if(num2 > num1 && num2 > num3){
+//   console.log(`O maior é ${num2}`);
+// }else{
+//   console.log(`O maior é ${num3}`);
+// }
+// Requisito 4
+// Declaração de Variáveis
+// let calculaPositivoNegativo = 0;
 
+// if (calculaPositivoNegativo < 0){
+//   console.log(`${calculaPositivoNegativo} é negativo`);
+// }else if (calculaPositivoNegativo > 0){
+//   console.log(`${calculaPositivoNegativo} é positivo`);
+// }else{
+//   console.log(`O valor é Zero`);
+// }
+// Requisito 5
+// let ladoA = 5;
+// let ladoB = 4;
+// let ladoC = 1;
+// let soma = ladoA + ladoB > ladoC && ladoB + ladoC > ladoA;
+
+// if (soma){
+//   console.log("É um triângulo");
+// }else{
+//   console.log("Erro! Não é um triângulo");
+// }
+// Requisito 6
+let pecaXadrez = "peão";
