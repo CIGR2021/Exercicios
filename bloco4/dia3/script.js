b259fe55bf5d5e92d06ffb0d9096793146073b5f
Carlos Vieira <appencigr@gmail.com>
2021-02-28 20:40:05 -0300

    Segundo Requisito completo!

--- bloco4/dia3/script.js
+++ bloco4/dia3/script.js
@@ -12,13 +12,38 @@ n = 5
 *****
 
 */
+// let n = 5;
+// if (n <= 1){
+  // console.log("Valor da variável 'n' tem que ser maior que 1");
+// }else{
+  // for (let index = 0; index < n; index += 1){
+    // let linha = "";
+    // for (let index = 0; index < n; index += 1){  
+      // linha += "*";
+    // }
+    // console.log(linha);
+  // }
+// }
+
+/* 2 - Para o segundo exercício, faça o mesmo que antes,
+mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+n = 5
+
+*
+**
+***
+****
+*****
+
+*/
 let n = 5;
 if (n <= 1){
   console.log("Valor da variável 'n' tem que ser maior que 1");
 }else{
+  let linha = "";
   for (let index = 0; index < n; index += 1){
-    let linha = "";
-    for (let index = 0; index < n; index += 1){  
+    for (let index = 4; index < n; index += 1){  
       linha += "*";
     }
     console.log(linha);
