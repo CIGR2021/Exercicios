509da9d22795859a5024c8fa76163644de85ff86
Carlos Vieira <appencigr@gmail.com>
2021-02-28 16:37:25 -0300

    Requisito 5 Completo!

--- bloco4/dia2/script.js
+++ bloco4/dia2/script.js
@@ -1,7 +1,6 @@
 // Requisito 1 - Mostrar todas as posições do Array
 // Requisito 2 - Somar todas as posições do Array
 // Requisito 3 - Calcular a Média
-// Requisito 4 - Comparar se média é maior ou menor que 20
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
@@ -20,8 +19,21 @@ console.log("-----------------------------!");
 console.log(`A soma do array é: ${soma}!`);
 console.log(`A média é: ${media}!`);
 
+// Requisito 4 - Comparar se média é maior ou menor que 20
 if(media < 20){
   console.log("É menor que 20!");
 }else{
   console.log("É maior que 20!");
 }
+// Requisito 5 - Descubra qual o maior valor contido no array e imprima-o
+let maiorValor = null;
+for(let index = 0; index < numbers.length; index += 1){
+  if (maiorValor < numbers[index]){
+    maiorValor = numbers[index]
+  }else{
+    maiorValor = maiorValor;
+  }
+}
+
+console.log(`O Maior número é: ${maiorValor}!`);
+// Requisito 6 - 
