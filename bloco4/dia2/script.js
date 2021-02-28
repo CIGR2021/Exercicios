c78b92c9c2102fc359926afa7792487b26858e75
Carlos Vieira <appencigr@gmail.com>
2021-02-28 16:20:09 -0300

    Requisidos 2 e 3 Completos!

--- bloco4/dia2/script.js
+++ bloco4/dia2/script.js
@@ -1,8 +1,21 @@
-// Requisito 1
+// Requisito 1 - Mostrar todas as posições do Array
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// 
+// for(let index = 0; index < numbers.length; index += 1){
+  // console.log(`Posição: ${index} elemento: ${numbers[index]}`);
+// }
+
+// Requisito 2 - Somar todas as posições do Array
+// Requisito 3 - Calcular a Média
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0;
 
 for(let index = 0; index < numbers.length; index += 1){
-  console.log(`Posição: ${index} elemento: ${numbers[index]}`);
+  soma += numbers[index];
 }
 
-// Requisito 2
+media = soma/numbers.length;
+
+console.log(`A soma do array é: ${soma}!`);
+console.log(`A média é: ${media}!`);
