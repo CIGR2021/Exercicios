a3f4e6103a65babe1abd031e4706f61723306c36
Carlos Vieira <appencigr@gmail.com>
2021-03-02 12:05:35 -0300

    Requisito 3 completo!

--- bloco4/dia4/script.js
+++ bloco4/dia4/script.js
@@ -1,6 +1,24 @@
 /*
   1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
-  Valor esperado no console: Bem-vinda, Margarida
+  Valor esperado no console: Bem-vinda, Margarida.
+  
+  2 - Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, 
+  imprima o objeto no console.
+  Valor esperado no console:
+    {
+      personagem: 'Margarida',
+      origem: 'Pato Donald',
+      nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+      recorrente: 'Sim'
+    };
+  3 - Faça um for/in que mostre todas as chaves do objeto.
+  Valor esperado no console:
+
+    personagem
+    origem
+    nota
+    recorrente
+  
 */
 let info = {
   personagem: "Margarida",
@@ -10,17 +28,16 @@ let info = {
 
 console.log(`Bem-vinda, ${info.personagem}!`);
 
-/*
-2 - Insira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, 
-imprima o objeto no console.
-Valor esperado no console:
-  {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: 'Sim'
-  };
-*/
+console.log("-------------------------");
 
 info.recorrente = "Sim";
 console.log(info);
+
+console.log("-------------------------");
+
+for (let key in info){
+  console.log(key)
+}
+
+console.log("-------------------------");
+
